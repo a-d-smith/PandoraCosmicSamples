a5c4bddcd923844bfa63c949ffcc85e4e15b4c5e
Andrew Smith <andrewsmithweb@gmail.com>
2016-11-26 11:40:50 -0600

    Fixed bug in setup script

--- setup/setup_all_v05_08_00_05.sh
+++ setup/setup_all_v05_08_00_05.sh
@@ -15,7 +15,7 @@ export USER_NAME=`whoami`
 export WORKING_DIR=`pwd`
 source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone.sh
 setup uboonecode v05_08_00_05 -q e9:prof
-source $WORKING_DIR/LArSoft-v05_08_00_05/localProducts_larsoft_v05_08_00_05_e9_prof/setup
+source $WORKING_DIR/LArSoft-v05_08_00_05/localProducts_larsoft_v05_08_00_e9_prof/setup
 cd $MRB_BUILDDIR
 mrbslp
 cd $WORKING_DIR
