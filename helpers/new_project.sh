a534a3ee53c960136600cdaf177881432b7bc50e
Andrew Smith <andrewsmithweb@gmail.com>
2016-11-26 15:51:14 -0600

    Updated the new project script

--- helpers/new_project.sh
+++ helpers/new_project.sh
@@ -36,12 +36,32 @@ cd $WORKING_DIR/projects/$name
 mkdir 1001
 cd 1001
 cp $WORKING_DIR/generic/xml/prod_chain_cosmic_generic.xml prod_chain_1001.xml
-sed -i -e 's/USER_NAME/'`echo $USER_NAME`'/g' prod_chain_1001.xml
-sed -i -e 's/PROJECT/'`echo $name`'/g' prod_chain_1001.xml
-sed -i -e 's/NUANCE_CODE/1001/g' prod_chain_1001.xml
-sed -i -e 's/WORKING_DIR/'`echo $WORKING_DIR`'/g' prod_chain_1001.xml
-sed -i -e 's/PRODGENIE_FCL/my_prodgenie_cosmic_ccqe/g' prod_chain_1001.xml
-
+sed -i -e 's,USER_NAME,'`echo $USER_NAME`',g' prod_chain_1001.xml
+sed -i -e 's,PROJECT,'`echo $name`',g' prod_chain_1001.xml
+sed -i -e 's,NUANCE_CODE,1001,g' prod_chain_1001.xml
+sed -i -e 's,WORKING_DIR,'`echo $WORKING_DIR`',g' prod_chain_1001.xml
+sed -i -e 's,PRODGENIE_FCL,my_prodgenie_cosmic_ccqe.fcl,g' prod_chain_1001.xml
 cd ..
 
 
+# Make a production chain xml file for 1003 nuance code
+mkdir 1003
+cd 1003
+cp $WORKING_DIR/generic/xml/prod_chain_cosmic_generic.xml prod_chain_1003.xml
+sed -i -e 's,USER_NAME,'`echo $USER_NAME`',g' prod_chain_1003.xml
+sed -i -e 's,PROJECT,'`echo $name`',g' prod_chain_1003.xml
+sed -i -e 's,NUANCE_CODE,1003,g' prod_chain_1003.xml
+sed -i -e 's,WORKING_DIR,'`echo $WORKING_DIR`',g' prod_chain_1003.xml
+sed -i -e 's,PRODGENIE_FCL,my_prodgenie_cosmic_res.fcl,g' prod_chain_1003.xml
+cd ..
+
+# Make a production chain xml file for 1001 nuance code
+mkdir 1004
+cd 1004
+cp $WORKING_DIR/generic/xml/prod_chain_cosmic_generic.xml prod_chain_1004.xml
+sed -i -e 's,USER_NAME,'`echo $USER_NAME`',g' prod_chain_1004.xml
+sed -i -e 's,PROJECT,'`echo $name`',g' prod_chain_1004.xml
+sed -i -e 's,NUANCE_CODE,1004,g' prod_chain_1004.xml
+sed -i -e 's,WORKING_DIR,'`echo $WORKING_DIR`',g' prod_chain_1004.xml
+sed -i -e 's,PRODGENIE_FCL,my_prodgenie_cosmic_res.fcl,g' prod_chain_1004.xml
+cd $WORKING_DIR
