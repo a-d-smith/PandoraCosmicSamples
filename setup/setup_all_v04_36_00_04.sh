d515acda802e30bbb0b6ff9555f3850ee77af4e7
Andrew Smith <andrewsmithweb@gmail.com>
2016-11-26 11:28:36 -0600

    Updated the setup scripts

--- setup/setup_all_v04_36_00_04.sh
+++ setup/setup_all_v04_36_00_04.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+# Make sure that the user is running from the repo
+if [ "`basename $(pwd)`" != "PandoraCosmicSamples" ]; then
+  echo "Oops: You need to run this script from the directory \"PandoraCosmicSamples\""
+  echo "      This is so I can setup your working area to be inside the repo you cloned from github!"
+  echo ""
+  echo "      You should be typing:"
+  echo "      source setup/setup_all_v04_36_00_04.sh"
+  echo ""
+  return
+fi
+
 export USER_NAME=`whoami`
-export WORKING_DIR=/uboone/app/users/$USER_NAME/gridsub
+export WORKING_DIR=`pwd`
 source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone.sh
 setup uboonecode v04_36_00_03 -q e9:prof
 source $WORKING_DIR/LArSoft-v04_36_00_03/localProducts_larsoft_v04_36_00_03_e9_prof/setup
 cd $MRB_BUILDDIR
 mrbslp
-cd $WORKING_DIR/LArSoft-v04_36_00_03
-source ../setup_grid.sh
+cd $WORKING_DIR
+source setup/setup_grid.sh
 
--- setup/setup_all_v05_08_00_05.sh
+++ setup/setup_all_v05_08_00_05.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+# Make sure that the user is running from the repo
+if [ "`basename $(pwd)`" != "PandoraCosmicSamples" ]; then
+  echo "Oops: You need to run this script from the directory \"PandoraCosmicSamples\""
+  echo "      This is so I can setup your working area to be inside the repo you cloned from github!"
+  echo ""
+  echo "      You should be typing:"
+  echo "      source setup/setup_all_v05_08_00_05.sh"
+  echo ""
+  return
+fi
+
 export USER_NAME=`whoami`
-export WORKING_DIR=/uboone/app/users/$USER_NAME/gridsub
+export WORKING_DIR=`pwd`
 source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone.sh
 setup uboonecode v05_08_00_05 -q e9:prof
-source $WORKING_DIR/LArSoft-v05_08_00_05/localProducts_larsoft_v05_08_00_e9_prof/setup
+source $WORKING_DIR/LArSoft-v05_08_00_05/localProducts_larsoft_v05_08_00_05_e9_prof/setup
 cd $MRB_BUILDDIR
 mrbslp
-cd $WORKING_DIR/LArSoft-v05_08_00_05
-source ../setup_grid.sh
-
-
+cd $WORKING_DIR
+source setup/setup_grid.sh
 
