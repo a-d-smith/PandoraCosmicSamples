26d6781f8764d7a07a5274108b39e59efed09710
Andrew Smith <andrewsmithweb@gmail.com>
2016-12-01 04:49:44 -0600

    Added an installer for LArSoft-v05_15_01

---
+++ install/install_dependencies_part3.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Make sure that the user is running from the repo
+if [ "`basename $(pwd)`" != "PandoraCosmicSamples" ]; then
+  echo "Oops: You need to run this script from the directory \"PandoraCosmicSamples\""
+  echo "      This is so I can setup your working area to be inside the repo you cloned from github!"
+  echo ""
+  echo "      You should be typing:"
+  echo "      source install/install_dependencies_part3.sh"
+  echo ""
+  return
+fi
+
+export WORKING_DIR=`pwd`
+
+# Install LArSoftv-06_15_01
+cd $WORKING_DIR
+echo "Installing LArSoft-v06_15_01"
+
+source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone.sh
+setup uboonecode v06_15_01 -q e9:prof
+mkdir LArSoft-v06_15_01
+cd LArSoft-v06_15_01
+mrb newDev
+cd $WORKING_DIR
