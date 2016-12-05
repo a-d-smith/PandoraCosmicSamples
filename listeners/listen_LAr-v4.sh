6d33c79d52f17662dfc936c427ac8bd3ce060871
Andrew Smith <andrewsmithweb@gmail.com>
2016-12-05 08:29:11 -0600

    Updated the listener

--- listeners/listen_LAr-v4.sh
+++ listeners/listen_LAr-v4.sh
@@ -25,7 +25,7 @@ totEvents=`cat $WORKING_DIR/projects/$project/$nuance/requestedEvents`
 echo 'Project: '$project
 echo '================================================================================'
 echo 'Nuance Code: '$nuance
-echo 'Requested Events:'$totEvents
+echo 'Requested Events: '$totEvents
 echo ''
 # GENIE stage
 echo '# Stage: GENIE'        
@@ -91,14 +91,74 @@ else
   echo 'Status: Waiting...'
 fi
 
+
+
+
+
 echo ''
 # G4 + Filter Stage
 echo '# Stage: G4 + Filter'        
 if [ $stage == 2 ]; then
-  echo 'G4'
+  if [ $action == 0 ]; then
+    # action = 0 -> Idle
+    echo 'Status: Idle'
+  elif [ $action == 1 ]; then
+    # action = 1 -> Submit
+    g4Events=`cat $WORKING_DIR/projects/$project/$nuance/g4Events | cut -f1 -d$'\n'`
+    g4Jobs=`cat $WORKING_DIR/projects/$project/$nuance/g4Events | cut -f2 -d$'\n'`
+    echo 'Status: Submitting '$g4Events' events over '$g4Jobs' jobs'
+    source $WORKING_DIR/helpers/job_submit_g4.sh $project $nuance
+    echo 2 > $WORKING_DIR/listeners/action
+  elif [ $action == 2 ]; then
+    # action = 2 -> Wait for jobs to finish
+    echo 'Status: Waiting for jobs to finish'
+    # Count the number of root files we have
+    find /pnfs/uboone/scratch/users/$USER_NAME/$project'_'$nuance/g4/*/lar.stat -type f > $WORKING_DIR/projects/$project/$nuance/g4Stat
+    g4JobsDone=0
+    g4JobsGood=0
+    while read line; do
+      if [ `cat $line` == 0 ]; then
+        g4JobsGood=$(($g4JobsGood + 1))  
+      fi
+      g4JobsDone=$(($g4JobsDone + 1))  
+    done < $WORKING_DIR/projects/$project/$nuance/g4Stat
+    g4Jobs=`cat $WORKING_DIR/projects/$project/$nuance/g4Events | cut -f2 -d$'\n'`
+    echo $g4JobsDone' jobs complete of a total '$g4Jobs'. '$g4JobsGood' were successful.'
+    if [ $g4JobsDone == $g4Jobs ]; then
+      echo 3 > $WORKING_DIR/listeners/action 
+    fi
+  elif [ $action == 3 ]; then
+    # action = 3 -> Check jobs
+    echo 'Status: Checking jobs for good events'
+    source $WORKING_DIR/helpers/job_check_g4.sh $project $nuance
+    echo 4 > $WORKING_DIR/listeners/action
+  elif [ $action == 4 ]; then
+    # action = 4 -> Prepare XML file for next stage
+    echo 'Status: Preparing for Detsim'
+    detsimEvents=0
+    while read line; do
+      n=`echo $line | cut -f2 -d' '`
+      detsimEvents=$(($detsimEvents + $n))
+    done < '/pnfs/uboone/scratch/users/'$USER_NAME'/'$project'_'$nuance'/g4/events.list'
+    detsimJobs=1
+    #detsimJobs=`bc -l <<< $detsimEvents'/10'`
+    #detsimJobs=`printf '%.*f' 0 $detsimJobs`
+    echo $detsimEvents > $WORKING_DIR/projects/$project/$nuance/detsimEvents
+    echo $detsimJobs >> $WORKING_DIR/projects/$project/$nuance/detsimEvents
+
+    sed -i '/<stage name="detsim">/a <numevents>'$detsimEvents'</numevents> \n <numjobs>'$detsimJobs'</numjobs>' $WORKING_DIR'/projects/'$project'/'$nuance'/prod_chain_'$nuance'.xml'
+
+    echo 1 > $WORKING_DIR/listeners/action
+    echo 3 > $WORKING_DIR/listeners/stage
+  fi
 else
   echo 'Status: Waiting...'
 fi
+
+
+
+
+
 
 echo ''
 # DetSim Stage
