32f1bf24b3f2a6f6c3d304fe06dc10118f94b096
Andrew Smith <andrewsmithweb@gmail.com>
2016-12-06 12:56:12 -0600

    Fixed a bug in the generic grid submission script

--- generic/sh/job_grid_signal_processing.sh
+++ generic/sh/job_grid_signal_processing.sh
@@ -3,7 +3,7 @@
 source /grid/fermiapp/products/uboone/setup_uboone.sh 
 setup uboonecode v05_08_00_05 -q e9:prof
 export USRDIR=WORKING_DIR/generic/fcl
-export INDIR=/pnfs/uboone/scratch/users/USER_NAME/PROJECT_NUANCE/detsim/IN_DIR
+export INDIR=IN_DIR
 export OUTDIR=/pnfs/uboone/scratch/users/USER_NAME/PROJECT_NUANCE/sig/INDEX
 
 ifdh cp ${INDIR}/prodgenie_bnb_nu_cosmic_uboone*.root my_file.root 
