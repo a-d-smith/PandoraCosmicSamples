495428b745722446bc25da23537c11273a6130c3
Andrew Smith <andrewsmithweb@gmail.com>
2016-12-06 10:33:38 -0600

    removed proxy from grid setup script

--- setup/setup_grid.sh
+++ setup/setup_grid.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 source /grid/fermiapp/products/common/etc/setups.sh
 setup jobsub_client
-kinit $USER_NAME
-kx509
-voms-proxy-init -noregen -rfc -voms fermilab:/fermilab/uboone/Role=Analysis
+#kinit $USER_NAME
+#kx509
+#voms-proxy-init -noregen -rfc -voms fermilab:/fermilab/uboone/Role=Analysis
