38e4281286efe24353da1c8d758bd7f0d9b28712
Andrew Smith <andrewsmithweb@gmail.com>
2016-12-06 18:07:27 -0600

    updated the signal processing submission shell script

--- generic/sh/job_grid_signal_processing.sh
+++ generic/sh/job_grid_signal_processing.sh
@@ -9,4 +9,4 @@ export OUTDIR=/pnfs/uboone/scratch/users/USER_NAME/PROJECT_NUANCE/sig/INDEX
 ifdh cp ${INDIR}/prodgenie_bnb_nu_cosmic_uboone*.root my_file.root 
 ifdh cp ${USRDIR}/my_signal_processing.fcl my_signal_processing.fcl 
 lar -c my_signal_processing.fcl -n -1 -s my_file.root 
-ifdh cp prodgenie*.root ${OUTDIR}/output_signal_processing.root
+ifdh cp *reco2.root ${OUTDIR}/output_signal_processing.root
